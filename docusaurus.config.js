4ee32d0089cefa9ec0410812fed9e114dfc1f51e
Henry <henry@rss3.io>
2022-06-08 15:07:55 +0100

    deps: upgrade

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "NaturalSelectionLabs",
-  projectName: "RSS3-Wiki",
+  projectName: "RSS3-Doc",
 
   i18n: {
     defaultLocale: "en",
