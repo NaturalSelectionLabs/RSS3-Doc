ad75def5ed342b793132d7425de49ad63e1320d2
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2022-06-09 02:04:36 +1200

    add: docusaurus openapi (#1)
    * add: swagger ui
    
    * feat: update packages
    
    * fix: whitepaper link
    
    * chore: remove resolutions
    
    * ref: remove unused tailwind
    
    * fix: homepage style
    
    * chore: fix title display
    
    * add: nav
    
    * fix: missing @docusaurus/types
    
    * fix: openapi spec
    
    Co-authored-by: Henry <henry@rss3.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,123 +1,134 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'RSS3',
-  tagline: 'RSS3 is an open information syndication protocol that aims to support efficient and decentralized information distribution in Web3.',
-  url: 'https://docs.rss3.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'https://graphics.rss3.workers.dev/',
+  title: "RSS3",
+  tagline:
+    "RSS3 is an open information syndication protocol that aims to support efficient and decentralized information distribution in Web3.",
+  url: "https://docs.rss3.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "https://graphics.rss3.workers.dev/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'NaturalSelectionLabs',
-  projectName: 'RSS3-Wiki',
+  organizationName: "NaturalSelectionLabs",
+  projectName: "RSS3-Wiki",
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      "docusaurus-preset-openapi",
+      /** @type {import('docusaurus-preset-openapi').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         blog: {
           showReadingTime: true,
-          path: 'changelog',
-          routeBasePath: 'changelog',
+          path: "changelog",
+          routeBasePath: "changelog",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        api: {
+          routeBasePath: "api", // todo update the api link
         },
       }),
     ],
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    /** @type {import('docusaurus-preset-openapi').ThemeConfig} */
     ({
       navbar: {
-        title: 'RSS3',
+        title: "RSS3",
         logo: {
-          alt: 'RSS3 Logo',
-          src: 'https://graphics.rss3.workers.dev/',
+          alt: "RSS3 Logo",
+          src: "https://graphics.rss3.workers.dev/",
         },
         items: [
           {
-            type: 'doc',
-            docId: '/category/guide',
-            position: 'right',
-            label: 'Docs',
+            to: "/docs/category/guide",
+            label: "Docs",
+            position: "right",
           },
-          { to: '/changelog', label: '🔄 API Changelog', position: 'right' },
-          { href: 'https://rss3.notion.site/Open-Source-Remote-RSS3-Offering-the-Dopest-Positions-b6fdbffee017449797397f45340de9d4', label: '💌 Join Us', position: 'right' },
-          // TODO: fix the link to the whitepaper
-          { to: './static/assets/RSS3-Whitepaper.pdf', label: 'Whitepaper', position: 'right' },
+          { to: "/changelog", label: "🔄 API Changelog", position: "right" },
           {
-            href: 'https://github.com/NaturalSelectionLabs/RSS3-Wiki',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://rss3.notion.site/Open-Source-Remote-RSS3-Offering-the-Dopest-Positions-b6fdbffee017449797397f45340de9d4",
+            label: "💌 Join Us",
+            position: "right",
+          },
+          {
+            to: "/assets/RSS3-Whitepaper.pdf",
+            label: "Whitepaper",
+            position: "right",
+            target: "_blank",
+          },
+          {
+            href: "https://github.com/NaturalSelectionLabs/RSS3-Wiki",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Products',
-                to: '/docs/category/products',
+                label: "Products",
+                to: "/docs/category/products",
               },
               {
-                label: 'PreGod API',
-                to: 'docs/category/rss3-pregod',
+                label: "PreGod API",
+                to: "docs/category/rss3-pregod",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/rss3_',
+                label: "Twitter",
+                href: "https://twitter.com/rss3_",
               },
               {
-                label: 'Discord',
-                href: 'https://discord.gg/rss3',
+                label: "Discord",
+                href: "https://discord.gg/rss3",
               },
               {
-                label: 'Telegram',
-                href: 'https://t.me/rss3_en',
+                label: "Telegram",
+                href: "https://t.me/rss3_en",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                href: 'https://rss3.notion.site/',
+                label: "Blog",
+                href: "https://rss3.notion.site/",
               },
               {
-                label: 'Branding',
-                to: '/docs/misc/branding',
+                label: "Branding",
+                to: "/docs/misc/branding",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/NaturalSelectionLabs',
+                label: "GitHub",
+                href: "https://github.com/NaturalSelectionLabs",
               },
             ],
           },
@@ -129,19 +140,6 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-  plugins: [
-    async function myPlugin(context, options) {
-      return {
-        name: "docusaurus-tailwindcss",
-        configurePostCss(postcssOptions) {
-          // Appends TailwindCSS and AutoPrefixer.
-          postcssOptions.plugins.push(require("tailwindcss"));
-          postcssOptions.plugins.push(require("autoprefixer"));
-          return postcssOptions;
-        },
-      };
-    },
-  ],
 };
 
 module.exports = config;
